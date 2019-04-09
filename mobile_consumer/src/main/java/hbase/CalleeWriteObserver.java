c2e38c902fc42ec053376617aab759d5fac20438
JesusJoey <joeyang@bu.edu>
2019-04-08 20:30:05 -0400

    Update Observer

--- mobile_consumer/src/main/java/hbase/CalleeWriteObserver.java
+++ mobile_consumer/src/main/java/hbase/CalleeWriteObserver.java
@@ -2,7 +2,6 @@ package hbase;
 
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Durability;
-import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
@@ -30,24 +29,12 @@ public class CalleeWriteObserver extends BaseRegionObserver{
 
         if(!targetTableName.equals(currentTableName)) return;
 
-public class CalleeWriteObserver extends BaseRegionObserver {
-    @Override
-    public void postPut(ObserverContext<RegionCoprocessorEnvironment> e, Put put, WALEdit edit, Durability durability) throws IOException {
-        super.postPut(e, put, edit, durability);
-        String targetTableName = PropertiesUtil.getProperty("hbase.mytopic.tablename");
-        String currentTableName = e.getEnvironment().getRegion().getRegionInfo().getTable().getNameAsString();
-
-        if(!targetTableName.equals(currentTableName)) {
-            return;
-        }
-
         String oriRowKey = Bytes.toString(put.getRow());
 
         String[] splitOriRowKey = oriRowKey.split("_");
 
         String oldFlag = splitOriRowKey[4];
         //如果当前插入的是被叫数据，则直接返回(因为默认提供的数据全部为主叫数据)
-
         if(oldFlag.equals("0")) return;
 
         int regions = Integer.valueOf(PropertiesUtil.getProperty("hbase.mytopic.regions"));
@@ -58,6 +45,7 @@ public class CalleeWriteObserver extends BaseRegionObserver {
         String flag = "0";
         String duration = splitOriRowKey[5];
         String regionCode = HBaseUtil.genRegionCode(callee, buildTime, regions);
+
         String calleeRowKey = HBaseUtil.genRowKey(regionCode, callee, buildTime, caller, flag, duration);
         //生成时间戳
         String buildTimeTs = "";
@@ -68,8 +56,6 @@ public class CalleeWriteObserver extends BaseRegionObserver {
         }
 
         // call1 call2 build_time build_time_ts
-        String calleeRowKey = HBaseUtil.genRowKey(regionCode, callee, buildTime, caller, flag, duration);
-
         Put calleePut = new Put(Bytes.toBytes(calleeRowKey));
         calleePut.addColumn(Bytes.toBytes("f2"), Bytes.toBytes("call1"), Bytes.toBytes(callee));
         calleePut.addColumn(Bytes.toBytes("f2"), Bytes.toBytes("call2"), Bytes.toBytes(caller));
@@ -77,25 +63,9 @@ public class CalleeWriteObserver extends BaseRegionObserver {
         calleePut.addColumn(Bytes.toBytes("f2"), Bytes.toBytes("flag"), Bytes.toBytes(flag));
         calleePut.addColumn(Bytes.toBytes("f2"), Bytes.toBytes("duration"), Bytes.toBytes(duration));
         calleePut.addColumn(Bytes.toBytes("f2"), Bytes.toBytes("build_time_ts"), Bytes.toBytes(buildTimeTs));
-        Bytes.toBytes(100L);
 
         Table table = e.getEnvironment().getTable(TableName.valueOf(targetTableName));
         table.put(calleePut);
-        table.close();
-    }
-}
-        try {
-            calleePut.addColumn(Bytes.toBytes("f2"), Bytes.toBytes("build_time_ts"),
-                    Bytes.toBytes(new SimpleDateFormat("yyyyMMddHHmmss").parse(buildTime).getTime()));
-        } catch (ParseException e1) {
-            calleePut.addColumn(Bytes.toBytes("f2"), Bytes.toBytes("build_time_ts"),
-                    Bytes.toBytes(""));
-            e1.printStackTrace();
-        }
-
-        Table table = e.getEnvironment().getTable(TableName.valueOf(targetTableName));
-        table.put(calleePut);
-
         table.close();
     }
 }
