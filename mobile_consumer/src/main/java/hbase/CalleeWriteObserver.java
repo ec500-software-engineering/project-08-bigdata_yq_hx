8ae6bbb50ec1f65aef8e0950659a09ff91270ede
JesusJoey <joeyang@bu.edu>
2019-04-08 20:09:43 -0400

    Update Observer

--- mobile_consumer/src/main/java/hbase/CalleeWriteObserver.java
+++ mobile_consumer/src/main/java/hbase/CalleeWriteObserver.java
@@ -2,10 +2,7 @@ package hbase;
 
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Durability;
-<<<<<<< HEAD
-=======
 import org.apache.hadoop.hbase.client.HTableInterface;
->>>>>>> a53569da901192a3913657a928cd4e83c081ce94
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
@@ -20,7 +17,6 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
-<<<<<<< HEAD
 public class CalleeWriteObserver extends BaseRegionObserver{
     SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
     @Override
@@ -33,7 +29,7 @@ public class CalleeWriteObserver extends BaseRegionObserver{
         String currentTableName = e.getEnvironment().getRegionInfo().getTable().getNameAsString();
 
         if(!targetTableName.equals(currentTableName)) return;
-=======
+
 public class CalleeWriteObserver extends BaseRegionObserver {
     @Override
     public void postPut(ObserverContext<RegionCoprocessorEnvironment> e, Put put, WALEdit edit, Durability durability) throws IOException {
@@ -44,33 +40,24 @@ public class CalleeWriteObserver extends BaseRegionObserver {
         if(!targetTableName.equals(currentTableName)) {
             return;
         }
->>>>>>> a53569da901192a3913657a928cd4e83c081ce94
 
         String oriRowKey = Bytes.toString(put.getRow());
 
         String[] splitOriRowKey = oriRowKey.split("_");
 
         String oldFlag = splitOriRowKey[4];
-<<<<<<< HEAD
         //如果当前插入的是被叫数据，则直接返回(因为默认提供的数据全部为主叫数据)
-=======
->>>>>>> a53569da901192a3913657a928cd4e83c081ce94
+
         if(oldFlag.equals("0")) return;
 
         int regions = Integer.valueOf(PropertiesUtil.getProperty("hbase.mytopic.regions"));
 
         String caller = splitOriRowKey[1];
         String callee = splitOriRowKey[3];
-<<<<<<< HEAD
-=======
-
->>>>>>> a53569da901192a3913657a928cd4e83c081ce94
         String buildTime = splitOriRowKey[2];
         String flag = "0";
         String duration = splitOriRowKey[5];
         String regionCode = HBaseUtil.genRegionCode(callee, buildTime, regions);
-<<<<<<< HEAD
-
         String calleeRowKey = HBaseUtil.genRowKey(regionCode, callee, buildTime, caller, flag, duration);
         //生成时间戳
         String buildTimeTs = "";
@@ -81,17 +68,14 @@ public class CalleeWriteObserver extends BaseRegionObserver {
         }
 
         // call1 call2 build_time build_time_ts
-=======
         String calleeRowKey = HBaseUtil.genRowKey(regionCode, callee, buildTime, caller, flag, duration);
 
->>>>>>> a53569da901192a3913657a928cd4e83c081ce94
         Put calleePut = new Put(Bytes.toBytes(calleeRowKey));
         calleePut.addColumn(Bytes.toBytes("f2"), Bytes.toBytes("call1"), Bytes.toBytes(callee));
         calleePut.addColumn(Bytes.toBytes("f2"), Bytes.toBytes("call2"), Bytes.toBytes(caller));
         calleePut.addColumn(Bytes.toBytes("f2"), Bytes.toBytes("build_time"), Bytes.toBytes(buildTime));
         calleePut.addColumn(Bytes.toBytes("f2"), Bytes.toBytes("flag"), Bytes.toBytes(flag));
         calleePut.addColumn(Bytes.toBytes("f2"), Bytes.toBytes("duration"), Bytes.toBytes(duration));
-<<<<<<< HEAD
         calleePut.addColumn(Bytes.toBytes("f2"), Bytes.toBytes("build_time_ts"), Bytes.toBytes(buildTimeTs));
         Bytes.toBytes(100L);
 
@@ -100,8 +84,6 @@ public class CalleeWriteObserver extends BaseRegionObserver {
         table.close();
     }
 }
-
-=======
         try {
             calleePut.addColumn(Bytes.toBytes("f2"), Bytes.toBytes("build_time_ts"),
                     Bytes.toBytes(new SimpleDateFormat("yyyyMMddHHmmss").parse(buildTime).getTime()));
@@ -117,4 +99,3 @@ public class CalleeWriteObserver extends BaseRegionObserver {
         table.close();
     }
 }
->>>>>>> a53569da901192a3913657a928cd4e83c081ce94
